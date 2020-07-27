77525273e5e157163ebaf8f11e275d8abb27eceb
jetnew <notesjet@gmail.com>
2020-07-27 17:09:56 +0800

    Cleaned

--- performance/performance.py
+++ performance/performance.py
@@ -2,7 +2,15 @@ import numpy as np
 
 
 def prob_overlap(y, y_hat, bins=5):
-    """Get probabilities of each bin, ignoring zeroes."""
+    """Get probabilities of each bin, ignoring zeroes.
+    
+    Computes the probabilities of multivariate histogram bins,
+    for the computation of empirical KL and JS divergences.
+    
+    Note: Ignores bins for both p and q if either p or q equals 0,
+    resulting in sometimes invalid computation of KL and JS divergences.
+    Either model smaller dimensions or improve the fit of the model.
+    """
     p, b = np.histogramdd(y, bins)
     q, _ = np.histogramdd(y_hat, b)
     p, q = p / y.shape[0], q / y.shape[0]
@@ -10,10 +18,10 @@ def prob_overlap(y, y_hat, bins=5):
     return p[tuple(idx)], q[tuple(idx)]
 
 def kl(p, q):
-    """Compute KL divergence."""
+    """Compute empirical KL divergence."""
     return np.sum(p * np.log(p / q))
 
 def js(p, q):
-    """Compute JS divergence."""
+    """Compute empirical JS divergence."""
     m = 0.5 * (p + q)
     return 0.5 * (kl(p, m) + kl(q, m))
